00aa6da939b98c2942f9de444cc9c9a956a255d8
John Sawiris <johnedwardsawiris@gmail.com>
2018-02-09 08:57:06 -0800

    create entry point file and configure webpack pro env and configure babel loader

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+const { resolve } = require('path');
+
+module.exports = {
+
+  entry: [
+    resolve(__dirname, 'src') + 'index.jsx'
+  ],
+
+  output: {
+    filename: 'app.bundle.js',
+    path: resolve(__dirname, 'build')
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            'es2015',
+            'react'
+          ]
+        }
+      }
+    ]
+  }
+}
