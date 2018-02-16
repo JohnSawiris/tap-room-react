86e943f7755f85b3760187a73759cdd038179646
John Sawiris <johnedwardsawiris@gmail.com>
2018-02-16 09:15:04 -0800

    change kegList component to a statful component

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -3,34 +3,49 @@ import Keg from './Keg';
 
 import kegs from './kegsData';
 
-function KegList() {
-  return(
-    <div className="kegs-wrap">
-      <h1>Available Kegs</h1>
-      <style jsx>{`
-          .kegs-wrap {
-            width: 100%;
-            padding: 1rem;
-          }
-          h1 {
-            text-align: center;
-          }
-          .keg-list {
-            display: flex;
-            width: 90%;
-            margin: 2rem auto 0;
-            justify-content: space-between;
-            flex-wrap: wrap;
-          }
-        `}</style>
-      <div className="keg-list">
-        {kegs.map((keg, i) =>
-          <Keg {...keg}
-            key={i}/>
-        )}
+class KegList extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state={
+      kegsList: kegs
+    }
+  }
+
+  handleAddingKegToList(newKeg) {
+    const copyKegsList = this.state.kegsList.slice();
+    this.setState({ kegsList: copyKegsList });
+  }
+
+  render() {
+    return(
+      <div className="kegs-wrap">
+        <h1>Available Kegs</h1>
+        <style jsx>{`
+            .kegs-wrap {
+              width: 100%;
+              padding: 1rem;
+            }
+            h1 {
+              text-align: center;
+            }
+            .keg-list {
+              display: flex;
+              width: 90%;
+              margin: 2rem auto 0;
+              justify-content: space-between;
+              flex-wrap: wrap;
+            }
+          `}</style>
+        <div className="keg-list">
+          {kegs.map((keg, i) =>
+            <Keg {...keg}
+              key={i}/>
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default KegList;
